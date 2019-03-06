c7079eec47a167a6ea2aedfe0b00aa13a5505022
Amr Keleg <amr_mohamed@live.com>
2019-03-06 22:34:04 +0200

    Mar 2019
    Add solutions to practice problems of CyberTalents

---
+++ CyberTalents/cryptography/assets/genfei/encrypt.py
@@ -0,0 +1,19 @@
+
+import sys
+from struct import pack, unpack
+
+def F(w):
+	return ((w * 31337) ^ (w * 1337 >> 16)) % 2**32
+
+def encrypt(block):
+	a, b, c, d = unpack("<4I", block)
+	for rno in xrange(32):
+		a, b, c, d = b ^ F(a | F(c ^ F(d)) ^ F(a | c) ^ d), c ^ F(a ^ F(d) ^ (a | d)), d ^ F(a | F(a) ^ a), a ^ 31337
+		a, b, c, d = c ^ F(d | F(b ^ F(a)) ^ F(d | b) ^ a), b ^ F(d ^ F(a) ^ (d | a)), a ^ F(d | F(d) ^ d), d ^ 1337
+	return pack("<4I", a, b, c, d)
+
+pt = open(sys.argv[1]).read()
+while len(pt) % 16: pt += "#"
+
+ct = "".join(encrypt(pt[i:i+16]) for i in xrange(0, len(pt), 16))
+open(sys.argv[1] + ".enc", "w").write(ct)
